bdd08989123b66d55c26c63a066fa65b0e2f7374
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-07 12:54:49 +0900

    フォント追加

--- header.php
+++ header.php
@@ -28,6 +28,19 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
 
     <link rel="stylesheet" href="<?php echo get_theme_file_uri('/css/reset.css'); ?>" />
+
+    <!-- 筑紫B丸ゴシック（Adobe font） -->
+    <script>
+      (function(d) {
+        var config = {
+          kitId: 'sfj8nak',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script>
+
     <link rel="stylesheet" href="<?php echo get_theme_file_uri('/css/style.css?'); ?>" />
     <title><?php echo esc_html( wp_get_document_title() ); ?></title>
 
