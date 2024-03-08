c18783add955f9e725a767ac2a99de7d8fc43469
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-08 13:31:03 +0900

    footer作成

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
     <footer id="footer">
       <div class="footer--inner">
 
-        <div class="footer--inner--top">
-          
+        <div class="footer--inner--left">
+
           <div class="footer_logo">
             <div>
               <img src="<?php echo get_theme_file_uri('/images/logo.png'); ?>" while="56" height="56" alt="ロゴ">
@@ -14,10 +14,35 @@
             <a href="tel:0643064308">TEL:06-4306-4308</a>
           </div><!-- /.footer_logo -->
 
+          <div class="footer_nav">
+            <ul>
+              <li><a href="#">えっぽについて</a></li>
+              <li><a href="#">お問い合わせ</a></li>
+              <li><a href="#">活動報告</a></li>
+              <li><a href="#">支援について</a></li>
+              <li><a href="#">たからばこ</a></li>
+              <li><a href="#">お知らせ</a></li>
+            </ul>
+          </div>
 
-        </div><!-- /.footer--inner--top -->
+        </div><!-- /.footer--inner--left -->
+
+        <div class="footer--inner--right">
+          <div class="footer_btn">
+              <a href="#">
+                <div class="btn btn-orange">
+                  <p>サポートのお願い</p>
+                </div>
+                <div class="btn btn-red">
+                  <p>メンバー＆サポーター募集</p>
+                </div>
+              </a>
+            </div><!-- /.footer_btn -->
+        </div><!-- /.footer--inner--right -->
 
       </div><!-- /.footer--inner -->
+
+      <small>&copy; eppo 2024</small>
     </footer>
 
     <?php wp_footer(); ?>
