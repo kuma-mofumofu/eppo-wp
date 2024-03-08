f00ca833c790832f80d712af77dd1fad57bddb37
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-08 10:45:53 +0900

    footerロゴ作成

--- footer.php
+++ footer.php
@@ -1,8 +1,21 @@
+    <div class="wood">
+      <img src="<?php echo get_theme_file_uri('/images/wood.png'); ?>" alt="木のイラスト">
+    </div>
     <footer id="footer">
       <div class="footer--inner">
-        <div>
 
-        </div>
+        <div class="footer--inner--top">
+          
+          <div class="footer_logo">
+            <div>
+              <img src="<?php echo get_theme_file_uri('/images/logo.png'); ?>" while="56" height="56" alt="ロゴ">
+            </div>
+            <p>早産児ビリルビン脳症による脳性まひ当事者の会えっぽ</p>
+            <a href="tel:0643064308">TEL:06-4306-4308</a>
+          </div><!-- /.footer_logo -->
+
+
+        </div><!-- /.footer--inner--top -->
 
       </div><!-- /.footer--inner -->
     </footer>
