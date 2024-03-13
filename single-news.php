8fd38d330158ab85165224e36d7be49b683d6480
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-13 16:28:59 +0900

    シングルページ

--- single-news.php
+++ single-news.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
 <?php  get_template_part('parts/header'); ?>
-<main>
+<main id="news_story">
+  <div class="title">
+    <h2>お知らせ</h2>
+  </div>
+
+  <div class="news_story--inner inner">
+    <h2><?php echo get_the_title(); ?></h2>
+
+    <?php the_content(); ?>
+
+  </div><!-- /.news_story--inner .inner -->
 
 </main>
-<?php wp_pagenavi(); ?>
 <?php  get_template_part('parts/contact'); ?>
 <?php get_footer(); ?>
